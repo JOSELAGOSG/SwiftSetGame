a46cb95a132e183bf8d329751e5286824005d525
José Lagos <joseantoniolagosg@gmail.com>
2023-08-28 16:13:10 -0300

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by José Lagos Guevara on 28-08-23.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
